1d1cddba2439d30d2a62dd28f7bb25ef9b9f5669
NewYorkKim <newyorkkim1030@gmail.com>
2023-07-31 14:03:54 +0900

    Feat: Update ScoreView for watchOS

--- TableTennisWatch Watch App/Views/ScoreView.swift
+++ TableTennisWatch Watch App/Views/ScoreView.swift
@@ -13,12 +13,58 @@ struct ScoreView: View {
     @ObservedObject var viewModel: ScoreViewModel
 //    @State var lastScore: Int = 0
     
+    enum Player {
+        case you
+        case partner
+        
+        var label: String {
+            switch self {
+            case .you:
+                return "You(ME)"
+            case .partner:
+                return "Partner"
+            }
+        }
+    }
+    
+    enum Symbol {
+        case person
+        case plus
+        case minus
+        
+        var name: String {
+            switch self {
+            case .person:
+                return "person.circle.fill"
+            case .plus:
+                return "plus"
+            case .minus:
+                return "minus"
+            }
+        }
+    }
+    
     var body: some View {
         VStack {
-            HStack {
-                Image(systemName: "person.circle.fill")
+            HStack(spacing: 6) {
+                HStack(spacing: 2){
+                    Text(Player.you.label)
+                        .font(.system(size: 13))
+                        .frame(width: 50, alignment: .trailing)
+                    Image(systemName: Symbol.person.name)
+                        .font(.system(size: 13))
+                }
+                
                 Text("\(viewModel.set1) - \(viewModel.set2)")
-                Image(systemName: "person.circle.fill")
+                    .fontWeight(.semibold)
+                
+                HStack(spacing: 2) {
+                    Image(systemName: Symbol.person.name)
+                        .font(.system(size: 13))
+                    Text(Player.partner.label)
+                        .font(.system(size: 13))
+                        .frame(width: 50, alignment: .leading)
+                }
             }
             HStack {
                 VStack {
@@ -26,7 +72,7 @@ struct ScoreView: View {
                         viewModel.plusScore(player: 0)
                         viewModel.session.sendMessage(["set1" : viewModel.set1], replyHandler: nil)
                     } label: {
-                        Image(systemName: "plus").bold()
+                        Image(systemName: Symbol.plus.name).bold()
                     }
                     .buttonStyle(PlusMinusButtonStyle())
                     
@@ -35,14 +81,14 @@ struct ScoreView: View {
                         .padding(.bottom, -10)
                         .frame(width: 85, height: 90)
                         .background(viewModel.servePlayer == 0
-                             ? LinearGradient(colors: [Color("Fills_Gradient_Start"), Color("Fills_Gradient_End")], startPoint: .top, endPoint: .bottom)
-                             : LinearGradient(colors: [.white.opacity(0.2)], startPoint: .top, endPoint: .bottom))
+                                    ? LinearGradient(colors: [Color("Fills_Gradient_Start"), Color("Fills_Gradient_End")], startPoint: .top, endPoint: .bottom)
+                                    : LinearGradient(colors: [.white.opacity(0.2)], startPoint: .top, endPoint: .bottom))
                         .cornerRadius(10)
                         .animation(.easeInOut, value: viewModel.set1)
                     Button {
                         viewModel.minusScore(player: 0)
                     } label: {
-                        Image(systemName: "minus").bold()
+                        Image(systemName: Symbol.minus.name).bold()
                     }
                     .buttonStyle(PlusMinusButtonStyle())
                 }
@@ -51,7 +97,7 @@ struct ScoreView: View {
                         viewModel.plusScore(player: 1)
                         viewModel.session.sendMessage(["set2" : viewModel.set2], replyHandler: nil)
                     } label: {
-                        Image(systemName: "plus").bold()
+                        Image(systemName: Symbol.plus.name).bold()
                     }
                     .buttonStyle(PlusMinusButtonStyle())
                     
@@ -60,14 +106,14 @@ struct ScoreView: View {
                         .padding(.bottom, -10)
                         .frame(width: 85, height: 90)
                         .background(viewModel.servePlayer == 1
-                             ? LinearGradient(colors: [Color("Fills_Gradient_Start"), Color("Fills_Gradient_End")], startPoint: .top, endPoint: .bottom)
-                             : LinearGradient(colors: [.white.opacity(0.2)], startPoint: .top, endPoint: .bottom))
+                                    ? LinearGradient(colors: [Color("Fills_Gradient_Start"), Color("Fills_Gradient_End")], startPoint: .top, endPoint: .bottom)
+                                    : LinearGradient(colors: [.white.opacity(0.2)], startPoint: .top, endPoint: .bottom))
                         .cornerRadius(10)
                         .animation(.easeInOut, value: viewModel.set2)
                     Button {
                         viewModel.minusScore(player: 1)
                     } label: {
-                        Image(systemName: "minus").bold()
+                        Image(systemName: Symbol.minus.name).bold()
                     }
                     .buttonStyle(PlusMinusButtonStyle())
                 }
@@ -76,7 +122,7 @@ struct ScoreView: View {
             .onChange(of: viewModel.player1) { newValue in
                 viewModel.session.sendMessage(["player1" : viewModel.player1], replyHandler: nil)
                 print("sessionTest : \(viewModel.player1)")
-                    WKInterfaceDevice.current().play(.notification)
+                WKInterfaceDevice.current().play(.notification)
             }
             .onChange(of: viewModel.player2) { newValue in
                 //                        WKInterfaceDevice.current().play(.notification)
