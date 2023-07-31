5eb52ae31f0bfa4449eff2281b01dd2851877a42
NewYorkKim <newyorkkim1030@gmail.com>
2023-07-31 09:54:03 +0900

    Feat: Update PlayResultView

--- TableTennisWatch Watch App/Views/PlayResultView.swift
+++ TableTennisWatch Watch App/Views/PlayResultView.swift
@@ -11,15 +11,39 @@ struct PlayResultView: View {
     
     @State private var isWin = true
     
+    enum Result {
+        case win
+        case lose
+        
+        var detail: (String, String) {
+            switch self {
+            case .win:
+                return ("Result_Win", "YOU WIN!")
+            case .lose:
+                return ("Result_Lose", "YOU LOSE!")
+            }
+        }
+    }
+    
     var body: some View {
         VStack {
-            Image(isWin ? "win" : "lose")
+            Image(showResult(isWin).0)
+                .resizable()
+                .frame(width: 60, height: 60)
                 .padding()
-            Text(isWin ? "YOU WIN!" : "YOU LOSE!")
+            Text(showResult(isWin).1)
                 .font(
-                Font.custom("SF Pro", size: 24)
-                .weight(.bold)
+                    .system(size: 24)
+                .weight(.semibold)
                 )
+        }
+    }
+    
+    func showResult(_ isWin: Bool) -> (String, String) {
+        if isWin {
+            return Result.win.detail
+        } else {
+            return Result.lose.detail
         }
     }
 }
